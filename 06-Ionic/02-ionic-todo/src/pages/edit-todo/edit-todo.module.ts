d4b109e01ccd52c596e28e504bb9497fde357750
Priidu Paomets <priidu.paomets@gmail.com>
2018-04-27 23:39:55 +0300

    Ionic demo rakendus ja IonicTodo rakenduse v1 täisversioon

---
+++ 06-Ionic/02-ionic-todo/src/pages/edit-todo/edit-todo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditTodoPage } from './edit-todo';
+
+@NgModule({
+  declarations: [
+    EditTodoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EditTodoPage),
+  ]
+})
+export class EditTodoPageModule {}
